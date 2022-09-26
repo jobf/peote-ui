4b4aaed0dda7d7e8db39d77b5d7299a71330072f
jf <j@b.f>
2022-09-26 23:48:43 +0100

    quick experiment

---
+++ samples/simple/src/SoftController.hx
@@ -0,0 +1,160 @@
+import peote.ui.event.PointerEvent;
+import peote.ui.interactive.UIElement;
+import peote.view.Color;
+import peote.ui.style.RoundBorderStyle;
+import lime.ui.Window;
+import haxe.CallStack;
+import peote.ui.PeoteUIDisplay;
+import peote.view.PeoteView;
+import lime.app.Application;
+
+class SoftController extends Application {
+	var peoteView:PeoteView;
+	var uiDisplay:PeoteUIDisplay;
+	var grid:Grid<Pad>;
+
+	override function onWindowCreate():Void {
+		switch (window.context.type) {
+			case WEBGL, OPENGL, OPENGLES:
+				try
+					startSample(window)
+				catch (_)
+					trace(CallStack.toString(CallStack.exceptionStack()), _);
+				default:
+					throw("Sorry, only works with OpenGL.");
+				}
+			}
+			
+	public function startSample(window:Window) {
+		peoteView = new PeoteView(window);
+		uiDisplay = new PeoteUIDisplay(20, 20, window.width - 40, window.height - 40, Color.GREY1);
+		peoteView.addDisplay(uiDisplay);
+		PeoteUIDisplay.registerEvents(window);
+		
+		var roundBorderStyle = new RoundBorderStyle();
+		roundBorderStyle.borderRadius = 3.0;
+		roundBorderStyle.color = 0x999999FF;
+		var padSize = 48;
+		var padGap = 6;
+		var gridWidth = 8;
+		var gridHeight = 4;
+
+		grid = new  Grid<Pad>((x:Int, y:Int) -> {
+			new Pad({
+				pixel_y: (y * padSize) + padGap,
+				pixel_x: (x * padSize) + padGap,
+				pixel_w: padSize,
+				pixel_h: padSize,
+				grid_y: y,
+				grid_x: x,
+				color_on: 0xffffffFF,
+				color_off: 0x999999FF,
+				color_hover: 0xee6666FF
+			}, roundBorderStyle,
+			(pressed:Pad) -> {
+				for(pad in grid.cells){
+					if(pad != pressed){
+						pad.toggle(false);
+					}
+				}
+			});
+		},
+		gridWidth,
+		gridHeight);
+		
+		for(pad in grid.cells){
+			uiDisplay.add(pad.element);
+		}
+	}
+
+}
+
+@:structInit
+class PadConfig {
+	public var pixel_x:Int;
+	public var pixel_y:Int;
+	public var pixel_w:Int;
+	public var pixel_h:Int;
+	public var grid_x:Int;
+	public var grid_y:Int;
+	public var color_hover:Int;
+	public var color_on:Int;
+	public var color_off:Int;
+}
+
+class Pad {
+	public var element(default, null):UIElement;
+	var isOn:Bool;
+	var config:PadConfig;
+	var notify_onClick:(pad:Pad)->Void;
+
+	public function new(config:PadConfig, style:RoundBorderStyle, notify_onClick:(pad:Pad)->Void) {
+		this.config = config;
+		this.notify_onClick = notify_onClick;
+		element = new UIElement(
+			config.pixel_x,
+			config.pixel_y,
+			config.pixel_w, 
+			config.pixel_h,
+			style);
+
+			element.onPointerOver = onOver;
+			element.onPointerOut = onOut;
+			element.onPointerClick = onClick;
+			element.upDownEventsBubbleToDisplay = true;
+		}
+
+	inline function onOver(uiElement:UIElement, e:PointerEvent) {
+		element.style.color = config.color_hover;
+		element.updateStyle();
+	}
+
+	inline function onOut(uiElement:UIElement, e:PointerEvent) {
+		element.style.color = isOn
+									? config.color_on
+									: config.color_off;
+		element.updateStyle();
+	}
+
+	inline function onClick(uiElement:UIElement, e:PointerEvent) {
+		toggle(!isOn);
+		notify_onClick(this);
+	}
+
+	public inline function toggle(isOn:Bool){
+		this.isOn = isOn;
+		element.style.color = isOn
+									? config.color_on
+									: config.color_off;
+		element.updateStyle();
+	}
+}
+
+class Grid<T> {
+	var width:Int;
+	var height:Int;
+
+	public var cells(default, null):Array<Null<T>>;
+
+	public function new(defaultValue:(x:Int, y:Int)->T, width:Int, height:Int) {
+		this.width = width;
+		this.height = height;
+		cells = [for (i in 0...width * height) defaultValue(x(i), y(i))];
+	}
+
+	inline function i(x:Int, y:Int) {
+		return x + width * y;
+	}
+
+	inline function x(index:Int):Int {
+		return Std.int(index % width);
+	}
+
+	inline function y(index:Int):Int {
+		return Std.int(index / width);
+	}
+
+	public function get(x:Int, y:Int):Null<T> {
+		return cells[i(x, y)];
+	}
+}
